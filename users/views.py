2df187d16dfdecee0051c742aac0462d54deb34f
njokuifeanyigerald <brainboyrichmond@gmail.com>
2021-06-05 17:48:52 +0100

    finished the view.py

--- users/views.py
+++ users/views.py
@@ -1,11 +1,12 @@
 from django import http
+from django.contrib.messages.api import success
 from django.db.models.signals import post_save
 from django.shortcuts import render,redirect, get_object_or_404
 from .models import Profile, FriendRequest
 # from feed.models import Post
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, login
 from django.conf import settings
 from django.http import HttpResponseRedirect
 from .forms import UserRegisterForm,UserUpdateForm,ProfileUpdateForm
@@ -139,17 +140,79 @@ def profile_view(request, slug):
     return render(request, 'user/profile.html', context)
 
 def register(request):
+    # used my own style
+    form = UserRegisterForm()
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages = 
+            messages,success(request,  f'your account has been created! you can login')
+            return redirect('login')
+    context = {
+        'form':form
+    }
+    return render(request, 'user/register.html', context)
 
+@login_required
+def edit_profile(request):
+    u_form = UserUpdateForm(instance=request.user)
+    p_form = ProfileUpdateForm(instance=request.user.profile)
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,  request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated')
+            return redirect('my_profile')
+    # used my own style
+    context = {
+        'u_form':u_form,
+        'p_form':p_form
+    }
+    return render(request, 'users/edit_profile.html',context)
+       
+@login_required
+def my_profile(request):
+    p = request.user.profile
+    you = p.user
+    sent_friend_requests = FriendRequest.objects.filter(from_user=you)
+    rec_friend_requests = FriendRequest.objects.filter(to_user = you)
+    # user_posts = Post.objects.filter(username=you)
+    friends = p.friends.all()
 
+    # is this user our friend
+    button_status = 'none'
+    if p not in request.user.profile.friends.all():
+        button_status= 'not friend'
 
+        # if we have sent him/her a friend request
+        if len(FriendRequest.objects.filter(from_user=request.user)
+        .filter(to_user=you)) ==1:
+            button_status = 'friend request sent'
 
+        # if he/she sent a friend request
 
+        if len(FriendRequest.objects.filter(from_user=you)
+        .filter(to_user=you)) ==1:
+            button_status =  'friend request received'
+        
+    context = {
+        'u': you,
+        'button_status':button_status,
+        'friend_list':friends,
+        'sent_friend_requests':sent_friend_requests,
+        'rec_friend_requests':rec_friend_requests,
+        # post_count: user_posts.count
+    }
+    return render(request, 'users/profile.html', context)
 
-
-
+@login_required
+def search_users(request):
+    query = request.GET.get('q')
+    object_list = User.objects.filter(username__icontains=query)
+    context = {
+        'users': object_list
+    }
+    return render(request, 'users/search.html', context)
