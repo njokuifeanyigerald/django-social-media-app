65d50979e1187266bb710b728611cb5463a771a0
njokuifeanyigerald <brainboyrichmond@gmail.com>
2021-06-08 17:10:24 +0100

    adding url

--- socialmediaapp/urls.py
+++ socialmediaapp/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('users.urls')),
+    path('', include('feed.urls')),
+    path('users/', include('users.urls')),
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,23 @@
 from django.urls import path
-from django.urls.conf import include
-from .views import users_list
+from django.urls import path
+from django.urls import path
+from .views import *
+from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
+
+
 
 urlpatterns = [
      path('', users_list, name='users_list'),
+     path('<slug>/', profile_view, name='profile_view'),
+     path('friend_request/send/<int:id>', send_friend_request, name='send_friend_request'),
+     path('friend',),
+     path('',),
+     path('', ),
+     path('',),
+     path('',),
+     path('', ),
+     path('',),
+     path('',),
 ]
