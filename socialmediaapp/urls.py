29d7018514d652ba08ddba54f65840a18b8637f0
njokuifeanyigerald <brainboyrichmond@gmail.com>
2021-06-13 13:23:22 +0100

    finished part 1

--- socialmediaapp/urls.py
+++ socialmediaapp/urls.py
@@ -15,9 +15,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users.views import *
+from django.contrib.auth import logout, views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('feed.urls')),
     path('users/', include('users.urls')),
+    path('search/',search_users, name='search_users'),
+    path('register/',register, name='register'),
+    path('login/',auth_views.LoginView.as_view(template_name="users/login.html"), name='login' ),
+    path('logout/', auth_views.LogoutView.as_view(template_name="users/logout.html"), name='logout'),
+    path('password-reset/',auth_views.PasswordResetView.as_view(template_name="users/password_rest.html"), name="password_reset"),
+    path('password-reset/done',auth_views.PasswordResetView.as_view(template_name="users/password_rest_done.html"), name="password_reset_done"),
+    path('password-reset/',auth_views.PasswordResetConfirmView.as_view(template_name="users/password_rest_confirm.html"), name="password_reset_confirm"),
+    path('password-reset/',auth_views.PasswordResetCompleteView.as_view(template_name="users/password_rest_complete.html"), name="password_reset_complete"),
+   
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,  document_root=settings.MEDIA_ROOT)
+    
--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from django.urls import path
 from django.urls import path
 from .views import *
-from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -12,12 +11,12 @@ urlpatterns = [
      path('', users_list, name='users_list'),
      path('<slug>/', profile_view, name='profile_view'),
      path('friend_request/send/<int:id>', send_friend_request, name='send_friend_request'),
-     path('friend',),
-     path('',),
-     path('', ),
-     path('',),
-     path('',),
-     path('', ),
-     path('',),
-     path('',),
+     path('friend_request/cancel/<int:id>',cancel_friend_request, name="cancel_friend_request" ),
+     path('friend_request/accept/<int:id>',accept_friend_request, name='accept_friend_request'),
+     path('friend_request/delete/<int:id>', delete_friend_request, name='delete_friend_request'),
+     path('friend/delete/<int:id>',delete_friend, name='delete_friend'),
+     path('friends/',friend_list, name='friend_list'),
+     path('edit_profile/', edit_profile, name='edit_profile' ),
+     path('my_profile', my_profile, name='my_profile'),
+    
 ]
